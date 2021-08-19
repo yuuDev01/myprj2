99eb64e3f52720d81bb9fcd4adb1fa6412a867da
yun <pasenoaz@gmail.com>
2021-08-19 14:49:57 +0900

    first commit

---
+++ src/main/java/com/example/myprj1/Myprj1Application.java
@@ -0,0 +1,13 @@
+package com.example.myprj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myprj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myprj1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myprj1/Myprj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myprj1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myprj1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
